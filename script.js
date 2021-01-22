236f0ace1c8322989dde95939a10768a56843bde
Dimitris Karakaxas <karakaxasdimitrios@yahoo.com>
2021-01-22 09:12:52 +0200

    ADD: images from external source

--- script.js
+++ script.js
@@ -1,35 +1,50 @@
+// Navigation Scroll feature
 const navigation = document.getElementById("navigation");
 
 window.addEventListener("scroll", () => {
     if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
-      navigation.style.transition = "background 0.15s";
+      navigation.style.transition = "background 0.2s ease-in";
       navigation.style.background = "rgba(20, 20, 20, 1)";
     } else {
       navigation.style.transition = "background 0.4s 0.15s";
-      navigation.style.background = "rgba(20, 20, 20, 0.1)";
+      navigation.style.background = "linear-gradient(to bottom, rgba(0, 0, 0, 0.7) 10%, rgba(0, 0, 0, 0)";
     }
 });
 
 
-fetch("https://api.themoviedb.org/3/discover/tv?api_key=19f84e11932abbc79e6d83f82d6d1045&with_networks=213")
-  .then(response => {
-    if(response.ok) return response.json();
-    throw new Error("Something Happened!");
-  })
-  .then(data => {
-    console.log(data);
-    addMovies(data.results);
-  })
-  .catch(error => {
-    console.log(error);
-  });
+const callFetch = function() {
+  const API_KEY = "92bcc12799d8068995c7c9650f414f3e";
+  addTvShows(API_KEY);
+}
 
+const addTvShows = function(API_KEY) {
+  fetch(`https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&sort_by=vote_average.desc&vote_count.gte=5000`)
+    .then(response => {
+      // console.log(response);
+      if(response.ok){
+        return response.json();
+      }else{
+        throw new Error("Something went wrong")
+      }
+    })
+    .then(data =>{
+      // console.log(data);
+      displayTvShows(data);
+    })
+    .catch(error =>{
+      console.log(error);
+    });
+}
 
-function addMovies(movies) {
-  const tvShowsEl= document.getElementById("tvShows");
-  tvShowsEl.innerHTML = "";
-  movies.forEach(movie => {
-    const html = `<img src="https://image.tmdb.org/t/p/original${movie.backdrop_path}" alt="">`
-    tvShowsEl.innerHTML += html;
+const displayTvShows = function(tvShows) {
+  console.log(tvShows);
+  const tvShowsEl = document.getElementById("tvShows");
+  tvShows.results.forEach(tvShow => {
+    const imgHtml = `<img src="https://image.tmdb.org/t/p/original${tvShow.backdrop_path}" alt="${tvShow.original_name}">`;
+    tvShowsEl.innerHTML += imgHtml;
   });
 }
+
+// function calls
+callFetch();
+

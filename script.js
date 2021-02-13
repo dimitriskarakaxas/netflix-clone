c2e373bc0ca9f79eb38eae2581579c5e7e4df8d9
Dimitris Karakaxas <karakaxasdimitrios@yahoo.com>
2021-02-13 14:00:44 +0200

    RESTARTING THE CLONE

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // Navigation Scroll feature
-const navigation = document.getElementById("navigation");
+// const navigation = document.getElementById("navigation");
 
-window.addEventListener("scroll", () => {
-  if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
-    navigation.style.transition = "background 0.2s ease-in";
-    navigation.style.background = "rgba(20, 20, 20, 1)";
-  } else {
-    navigation.style.transition = "background 0.4s 0.15s";
-    navigation.style.background = "linear-gradient(to bottom, rgba(0, 0, 0, 0.7) 10%, rgba(0, 0, 0, 0)";
-  }
-});
+// window.addEventListener("scroll", () => {
+//   if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
+//     navigation.style.transition = "background 0.2s ease-in";
+//     navigation.style.background = "rgba(20, 20, 20, 1)";
+//   } else {
+//     navigation.style.transition = "background 0.4s 0.15s";
+//     navigation.style.background = "linear-gradient(to bottom, rgba(0, 0, 0, 0.7) 10%, rgba(0, 0, 0, 0)";
+//   }
+// });
 
 
 const alreadyDisplayed = [];

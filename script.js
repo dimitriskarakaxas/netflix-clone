118c8a346dc9c1dd39c2cbd8edcc820e7229ac4c
dimitriskarakaxas <karakaxasdimitrios@yahoo.com>
2021-03-13 08:23:40 +0200

    ADD: Fetch Movies from TMDB API

--- script.js
+++ script.js
@@ -1,72 +1,48 @@
-// Navigation Scroll feature
-// const navigation = document.getElementById("navigation");
+const navigationEl = document.querySelector('.navigation');
 
-// window.addEventListener("scroll", () => {
-//   if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
-//     navigation.style.transition = "background 0.2s ease-in";
-//     navigation.style.background = "rgba(20, 20, 20, 1)";
-//   } else {
-//     navigation.style.transition = "background 0.4s 0.15s";
-//     navigation.style.background = "linear-gradient(to bottom, rgba(0, 0, 0, 0.7) 10%, rgba(0, 0, 0, 0)";
-//   }
-// });
+const displayedMovies = [];
 
+const fetchData = (URL, id) => {
+    fetch(URL)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Error: Something went wrong...");
+            } else {
+                return response.json();
+            }
+        })
+        .then((data) => {
+            // console.log(data);
+            displayMovies(data, id);
 
-const alreadyDisplayed = [];
-
-const getPopular = function () {
-  const popularURL = "https://api.themoviedb.org/3/discover/tv?api_key=92bcc12799d8068995c7c9650f414f3e&sort_by=vote_average.desc&vote_count.gte=5000";
-  callFetch(popularURL, "popular");
+        })
+        .catch((error) => {
+            console.log(error)
+        });
 }
 
-const getTrending = function () {
-  const trendingURL = "https://api.themoviedb.org/3/trending/tv/week?api_key=92bcc12799d8068995c7c9650f414f3e";
-  callFetch(trendingURL, "trending")
-}
+const displayMovies = (data, id) => {
+    const containerEl = document.getElementById(id);
 
-const callFetch = function (URL, id) {
-  console.log(URL);
-  fetch(URL)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error("Something went wrong")
-      } else {
-        return response.json();
-      }
+    data.results.forEach((result) => {
+        const { backdrop_path: img, id: imgId } = result;
+        if (alreadyDisplayed(imgId)) {
+            const imageEl = `<img src="https://image.tmdb.org/t/p/original/${img}" alt="">`;
+            containerEl.innerHTML += imageEl;
+        }
     })
-    .then(data => {
-      displayMovies(data, id);
-    })
-    .catch(error => {
-      console.log(error);
-    });
 }
 
-// Check if a movie is already displayed
-const movieIsNotDisplayed = function (movieID) {
-  // push movie_id in the array when is NOT DISPLAYED
-  if (!alreadyDisplayed.includes(movieID)) {
-    alreadyDisplayed.push(movieID);
-    return true; // When the movie is not in the [alreadyDisplayed] array
-  }
-  return false; // When the movie is already displayed
-}
 
-const displayMovies = function (data, id) {
-  const containerEL = document.getElementById(id);
-
-  data.results.forEach(movie => {
-    if (movieIsNotDisplayed(movie.id)) {
-      const imageEl = ` <img src="https://image.tmdb.org/t/p/original/${movie.backdrop_path}" alt="${id}">`;
-      containerEL.innerHTML += imageEl;
+const alreadyDisplayed = (imgID) => {
+    if (!displayedMovies.includes(imgID)) {
+        displayedMovies.push(imgID);
+        return true;
+    } else {
+        return false;
     }
-  });
-  console.log(alreadyDisplayed);
-};
+}
 
+fetchData('https://api.themoviedb.org/3/discover/tv?api_key=92bcc12799d8068995c7c9650f414f3e&sort_by=popularity.desc&vote_count.gte=5000', 'row-1');
 
-getPopular();
-getTrending();
-
-//  https://api.themoviedb.org/3/genre/movie/list?api_key=<<api_key>>&language=en-US
-
+fetchData('https://api.themoviedb.org/3/trending/tv/week?api_key=92bcc12799d8068995c7c9650f414f3e', 'row-2');
